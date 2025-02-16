97aef67f1dff2877e17963529226562665d46c98
NandhiniS <s311nandhini@gmail.com>
2025-02-16 14:09:19 +0530

    More on Strings.

--- OOPs - Inheritance/src/StringInspectionMethods.java
+++ OOPs - Inheritance/src/StringInspectionMethods.java
@@ -1,15 +1,18 @@
 public class StringInspectionMethods {
     public static void main(String[] args) {
-        printInformation("Nandhini");
+        printInformation("Nandhini Says Hoi");
         printInformation("");
         printInformation("\t     \n");
 
-        String HellowWorld = "Hello World";
+        String HellowWorld = "Hello World Let's Code";
         System.out.printf("index of r = %d %n", HellowWorld.indexOf('r'));
         System.out.printf("index of World = %d %n", HellowWorld.indexOf("World"));
 
         System.out.printf("index of o = %d %n", HellowWorld.indexOf('o'));
         System.out.printf("index of o = %d %n", HellowWorld.lastIndexOf('o'));
+
+        System.out.printf("index of o = %d %n", HellowWorld.indexOf('o', 3)); //starts looking from index 3.
+        System.out.printf("index of o = %d %n", HellowWorld.lastIndexOf('o', 8)); //starts looking from index 8
     }
 
     public static void printInformation(String string){
